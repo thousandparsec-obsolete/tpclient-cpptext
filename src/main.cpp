33c1eafc3de90dc58a71afc49d38c378a0f2a007
Lee Begg <lee@thousandparsec.net>
2004-11-06 07:41:57 +0000

    Added main loop and help, quit/exit, connect and disconnect.

--- src/main.cpp
+++ src/main.cpp
@@ -25,8 +25,40 @@ int main(int argc, char** argv){
   //async frame listener
   //logger
 
-
+  bool happy = true;
   // main loop
+  while(happy && !std::cin.eof()){
+    std::cout << "> " << std::flush;
+
+    std::string command;
+    std::cin >> std::ws >> command;
+
+    if(command == "help"){
+      std::cout << "Help" << std::endl;
+      std::cout << "  Known commands are:" << std::endl;
+      std::cout << "\thelp - this help" << std::endl;
+      std::cout << "\tquit, exit - exits tpclient-cpptext" << std::endl;
+      std::cout << "\tconnect - connect to server (takes one arg)" << std::endl;
+      std::cout << "\tdisconnect - disconnect from server" << std::endl;
+    }else if(command == "quit" || command == "exit"){
+      happy = false;
+    }else if(command == "connect"){
+      std::string host;
+      std::cin >> std::ws >> host;
+      sock->setServerAddr(host.c_str());
+      fc->connect();
+    }else if(command == "disconnect"){
+      if(fc->getStatus() != 0){
+	fc->disconnect();
+      }
+    }
+    
+    if(fc->getStatus() > 2){
+      fc->pollForAsyncFrames();
+    }
+
+  }
+
   
   fc->disconnect();
   delete fc; // also takes care of sock for us
