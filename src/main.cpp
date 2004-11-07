2b5bda104454ed3d06edc187df5935290435feae
Lee Begg <lee@thousandparsec.net>
2004-11-07 03:28:11 +0000

    Gets objects and board, but doesn't display them yet.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 
 #include <tpproto/framecodec.h>
 #include <tpproto/tcpsocket.h>
+#include <tpproto/object.h>
+#include <tpproto/board.h>
+#include <tpproto/getobjectbyid.h>
+#include <tpproto/getboard.h>
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -31,8 +35,22 @@ int main(int argc, char** argv){
   fc->setLogger(new PrintLogger());
 
   bool happy = true;
+  Object* currObj = NULL;
+  Board* currBoard = NULL;
+  bool brdobj = true;
+
   // main loop
   while(happy && !std::cin.eof()){
+    if(brdobj){
+      if(currObj != NULL){
+	std::cout << "Object: " << currObj->getName();
+      }
+    }else{
+      if(currBoard != NULL){
+	std::cout << "Board: " << currBoard->getName();
+      }
+    }
+
     std::cout << "> " << std::flush;
 
     std::string command;
@@ -47,6 +65,12 @@ int main(int argc, char** argv){
       std::cout << "\tdisconnect - disconnect from server" << std::endl;
       std::cout << "\tlogin - login to server (takes two args)" << std::endl;
       std::cout << "\ttime - get the time before the next End Of Turn" << std::endl;
+      std::cout << "\tobject - get an object, take an id arg" << std::endl;
+      std::cout << "\tboard - get a board, take an id arg" << std::endl;
+      std::cout << std::endl << "When working on an Object (prompt starts with \"Object\"):"<< std::endl;
+
+      std::cout << std::endl << "When working on a Board (prompt starts with \"Board\"):"<< std::endl;
+
     }else if(command == "quit" || command == "exit"){
       happy = false;
     }else if(command == "connect"){
@@ -54,9 +78,25 @@ int main(int argc, char** argv){
       std::cin >> std::ws >> host;
       sock->setServerAddr(host.c_str());
       fc->connect();
+      if(currObj != NULL){
+	delete currObj;
+	currObj = NULL;
+      }
+      if(currBoard != NULL){
+	delete currBoard;
+	currBoard = NULL;
+      }
     }else if(command == "disconnect"){
       if(fc->getStatus() != 0){
 	fc->disconnect();
+      }
+      if(currObj != NULL){
+	delete currObj;
+	currObj = NULL;
+      }
+      if(currBoard != NULL){
+	delete currBoard;
+	currBoard = NULL;
       }
     }else if(command == "login"){
       std::string user, pass;
@@ -87,6 +127,39 @@ int main(int argc, char** argv){
       
       std::cout << tvrem << " seconds." << std::endl;
       
+    }else if(command == "object"){
+      int id;
+      std::cin >> id;
+      GetObjectByID* go = fc->createGetObjectByIDFrame();
+      go->addObjectID(id);
+      Object* ob = fc->getObjects(go).begin()->second;
+      if(ob == NULL){
+	std::cout << "Did not get object" << std::endl;
+      }else{
+	if(currObj != NULL){
+	  delete currObj;
+	}
+	currObj = ob;
+	brdobj = true;
+      }
+      
+
+    }else if(command == "board"){
+      int id;
+      std::cin >> id;
+      GetBoard* gb = fc->createGetBoardFrame();
+      gb->addBoardId(id);
+      Board* bb = fc->getBoards(gb).begin()->second;
+      if(bb == NULL){
+	std::cout << "Did not get Board" << std::endl;
+      }else{
+	if(currBoard != NULL){
+	  delete currBoard;
+	}
+	currBoard = bb;
+	brdobj = false;
+
+      }
     }
 
     
@@ -97,6 +170,12 @@ int main(int argc, char** argv){
 
   }
 
+  if(currObj != NULL){
+    delete currObj;
+  }
+  if(currBoard != NULL){
+    delete currBoard;
+  }
   
   fc->disconnect();
   delete fc; // also takes care of sock for us
